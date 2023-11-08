2b027bbc78d689c35c84107224f73b0fbdd92f95
Simran Mandal <simranmandal0211@gmail.com>
2023-11-09 00:24:17 +0530

    Add more functionality in authReducers and remove authContext

--- src/Redux/Reducers/authReducer.js
+++ src/Redux/Reducers/authReducer.js
@@ -13,6 +13,106 @@ import 'react-toastify/dist/ReactToastify.css';
 // for list of all the users, if a user is  logged, data of loggedIn user
 const initialState={ userList: [], isLoggedIn: false, userLoggedIn: null };
 
+
+// Async thunk for getting list of all the users within the firebase database
+export const getInitialUserList = createAsyncThunk(
+    "auth/userList", (args, thunkAPI) => {
+        // getting datta from firebase
+        const unsub = onSnapshot(collection(db, "buybusy-redux"), (onSnapshot) => {
+            const users = snapShot.docs.map((doc) => {
+                return {
+                    id: doc.id,
+                    ...doc.data()
+                }
+            });
+
+            // storing the userList inside initialState variable
+            thunkAPI.dispatch(setUserList(users));
+        });
+    }
+);
+
+
+
+// AsyncThunk for creating new user in datatbase
+export const createUserThunk = createAsyncThunk(
+    "auth/createUser",
+    async (data, thunkAPI) => {
+        // getting userList from initialState
+        const { authReducer } = thunkAPI.getState();
+        const { userList } = authReducer;
+
+        // checking whether user's email already exist or not
+        const index = userList.findIndex((user) => user.email === data.email);
+
+        // if email address already exist or not
+        if(index !== -1){
+            toast.error('Email address already in use !!');
+            return;
+        }
+
+
+        // if email not found create new user 
+        const docRef =await addDoc(collection(db, "buybusy-redux"), {
+            name:data.name,
+            email:data.email,
+            password:data.password,
+            cart:[],
+            orders:[]
+        });
+        // notification 
+        toast.success("New user Created, Please LogIn to Continue !!");
+    }
+)
+
+
+// AsyncThunk for signIn user
+export const createSessionThunk = createAsyncThunk(
+    " auth/createSession",
+    async (data, thunkAPI) => {
+        // getting userList from initialState
+        const { authReducer } = thunkAPI.getState();
+        const { userList } = authReducer;
+
+        // finding user inside the userList
+        const index = userList.findIndex((user) => user.email === data.email);
+
+        if(index === -1){
+            toast.error("Email does not exist, Try again or SignUp Insted!!!");
+            return false;
+        }
+
+        // if email found in database then metch password
+        if(userList[index].password === data.password){
+            toast.success("Sign In Successfully!!!");
+
+            // Logging in user and storing its data in local variable
+            thunkAPI.dispatch(setLoggedIn(true));
+            thunkAPI.dispatch(setUserLoggedIn(userList[index]));
+
+            // generating user's login token and stire user's data
+            window.localStorage.setItem("token", true);
+            window.localStorage.setItem("index", JSON.stringify(userList[index]));
+            return true;
+        }
+        else{
+            // if password does'nt match in database
+            toast.error("wrong UaseNAme/Password, Try Again");
+            return false;
+        }
+    }
+);
+
+// AsyncThunk for SignOut
+export const removeSessionThunk = createAsyncThunk(
+    "auth/removeSession", () => {
+        // removing user data and token from local storage
+        window.localStorage.removeItem("token");
+        window.localStorage.removeItem("index");
+    }
+)
+
+
 const authSlice = createSlice({
     name: 'authentication',
     initialState,
@@ -23,13 +123,22 @@ const authSlice = createSlice({
         },
         // whether user isLoggedIn or not
         setLoggedIn: (state, action) => {
-            state.isLoggedI = action.payload;
+            state.isLoggedIn = action.payload;
         },
         // data of loggedIn user
         setUserLoggedIn: (state,action) => {
             state.userLoggedIn = action.payload;
         }
     },
+
+
+    extraReducers: (builder) => {
+        builder.addCase(removeSessionThunk.fulfilled, (state, action) => {
+            state.isLoggedIn = false;
+            state.userLoggedIn = null;
+            toast.success("Sign Out Successfully!!!");
+        })
+    }
 });
 
 
